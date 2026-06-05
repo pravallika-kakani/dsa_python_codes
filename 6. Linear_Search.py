b2dd80db636fbcf793cd3e63ba47f2e71e4d3a0a
pravallika-kakani <nagapravallikakakani@gmail.com>
2026-06-05 19:56:04 +0530

    Add files via upload

---
+++ 6. Linear_Search.py
@@ -0,0 +1,10 @@
+def linear(arr, target):
+    for i in range(len(arr)):
+        if(arr[i] == target):
+            return i
+    return -1
+arr = [12, 1, 23, 45, 90, 11, 9]
+target = 23
+print(linear(arr, target))
+target = 34
+print(linear(arr, target))
