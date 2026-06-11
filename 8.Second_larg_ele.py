9ce9dcbef7810d97ea82e10d58bca614907f8ccb
pravallika-kakani <nagapravallikakakani@gmail.com>
2026-06-11 19:23:19 +0530

    Add files via upload

---
+++ 8.Second_larg_ele.py
@@ -0,0 +1,11 @@
+nums =[55, 32, 97, -55, 45, 32, 88, 21, 97]
+n = len(nums)
+largest =float('-inf')
+second_l= float('-inf')
+for i in range(0,n):
+    if(nums[i]>largest):
+        second_l = largest
+        largest = nums[i]
+    elif(nums[i]> second_l and nums[i]!=largest):
+        second_l = nums[i]
+print(second_l)
